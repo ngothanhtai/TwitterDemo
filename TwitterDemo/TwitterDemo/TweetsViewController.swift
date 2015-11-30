79bd09f34d618e15bd736c799326179d69deda20
Ngo Thanh Tai <ngothanhtai89@yahoo.com>
2015-11-30 12:42:09 +0700

    Infinite loading

--- TwitterDemo/TwitterDemo/TweetsViewController.swift
+++ TwitterDemo/TwitterDemo/TweetsViewController.swift
@@ -12,6 +12,9 @@ class TweetsViewController: UIViewController {
     @IBOutlet weak var tableView:UITableView!
     var tweets: [Tweet] = [Tweet]()
     var refreshControl:UIRefreshControl!
+    var maxId:String?
+    var loadingMoreResult = false
+    var loadingViewIndicator:UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,35 +22,78 @@ class TweetsViewController: UIViewController {
 
         fetchData()
     }
-
-    func fetchData() {
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
         
-        self.tableView.hidden = tweets.count == 0
-        refreshControl.beginRefreshing()
-        TwitterClient.sharedInstance.homeTimelineWithParams(nil) { (tweets, error) -> () in
+        self.loadingViewIndicator.hidden = true
+        self.loadingViewIndicator.stopAnimating()
+        self.loadingMoreResult = false
+
+        self.tableView.reloadData()
+    }
+
+    func fetchData(maxId:String = "") {
+
+        var params:NSDictionary = NSDictionary()
+        if maxId.isEmpty == false {
+            params = [ "max_id": maxId]
+        } else {
+            refreshControl.beginRefreshing()
+        }
+        
+        TwitterClient.sharedInstance.homeTimelineWithParams(params) { (tweets, error) -> () in
             if error != nil {
-                print("homeTimeline", error)
+                if let error = error,
+                let data = error.userInfo["com.alamofire.serialization.response.error.data"] as? NSData
+                {
+                    let dic = try! NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments) as! NSDictionary
+                    print(dic)
+                    if let errors = dic["errors"],
+                    let error = errors[0],
+                    let message = error["message"] {
+                        self.showAlert(title: "Error message", message: message as! String)
+                    }
+                }
                 return
             }
-            self.tableView.hidden = false
-            self.tweets = tweets!
+            
+            if maxId.isEmpty == false {
+                if var tweets = tweets {
+                    tweets.removeFirst()
+                    self.tweets.appendContentsOf(tweets)
+                }
+            } else {
+                self.tweets = tweets!
+            }
+            self.loadingMoreResult = false
             
             self.tableView.reloadData()
             
             self.refreshControl.endRefreshing()
             
+            self.updateMaxId()
+            
+            self.loadingViewIndicator.hidden = true
+            self.loadingViewIndicator.stopAnimating()
+            
         }
     }
     
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(true)
-        self.tableView.reloadData()
+    func showAlert(title title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alertController.addAction(okAction)
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    func updateMaxId() {
+        if let lastTweet = self.tweets.last {
+            self.maxId = lastTweet.id
+        }
     }
     
     func initControls() {
-        
-        
-
         tableView.dataSource = self
         tableView.delegate = self
         
@@ -56,9 +102,35 @@ class TweetsViewController: UIViewController {
         
         refreshControl = UIRefreshControl()
         refreshControl.endRefreshing()
-        refreshControl.addTarget(self, action: "fetchData", forControlEvents: .ValueChanged)
+        refreshControl.addTarget(self, action: "onRefresh", forControlEvents: .ValueChanged)
         
         self.tableView.insertSubview(refreshControl, atIndex: 0)
+        
+        self.addLoadingViewIndicatorAtBottomOfTableView()
+    }
+    
+    func onRefresh() {
+        self.fetchData()
+    }
+    
+    func addLoadingViewIndicatorAtBottomOfTableView() {
+        // add trigger at the end icon
+        let tableViewFooter = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
+        self.loadingViewIndicator = UIActivityIndicatorView(activityIndicatorStyle: .Gray)
+        self.loadingViewIndicator.hidden = true
+        self.loadingViewIndicator.stopAnimating()
+        self.loadingViewIndicator.center = tableViewFooter.center
+        
+        tableViewFooter.addSubview(self.loadingViewIndicator)
+        self.tableView.tableFooterView = tableViewFooter
+        
+        self.loadingViewIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        let views = ["view": tableViewFooter, "newView": self.loadingViewIndicator]
+        let horizontalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("H:[view]-(<=0)-[newView(320)]", options: NSLayoutFormatOptions.AlignAllCenterY, metrics: nil, views: views)
+        view.addConstraints(horizontalConstraints)
+        let verticalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("V:[view]-(<=0)-[newView(50)]", options: NSLayoutFormatOptions.AlignAllCenterX, metrics: nil, views: views)
+        view.addConstraints(verticalConstraints)
     }
 
     @IBAction func logout(sender:AnyObject) {
@@ -93,6 +165,14 @@ extension TweetsViewController:UITableViewDataSource, UITableViewDelegate {
         let cell = tableView.dequeueReusableCellWithIdentifier("TwitterCell", forIndexPath: indexPath) as! TweetTableViewCell
         cell.updateUI(self.tweets[indexPath.row])
         cell.delegate = self
+        
+        if self.loadingMoreResult == false && self.loadingViewIndicator.hidden && indexPath.row == self.tweets.count - 1{
+            self.loadingViewIndicator.hidden = false
+            self.loadingViewIndicator.startAnimating()
+            self.fetchData(self.maxId!)
+            self.loadingMoreResult = true
+        }
+        
         return cell
     }
     
